016c983eb5acd1092bb7266c35a1d40f750bab0c
Aleksandr <kholopchik@gmail.com>
2023-08-24 16:08:28 +0300

    исправлен codestyle

--- src/main/java/ru/yandex/practicum/filmorate/service/ReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReleaseDate.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.service;
 
-
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
@@ -8,6 +7,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 @Documented
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
